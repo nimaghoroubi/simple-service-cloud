1f9c3d20d4fabfbbd999c05485abc5cd6e87dcdc
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-03 19:32:38 +0200

    adding env setter for fresh installs

---
+++ env_set.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "installing updates"
+sudo apt-get update &&
+sudo apt-get -y upgrade &&
+echo "installing python"
+sudo apt-get -y install python &&
+echo "installing and upgrading pip"
+sudo apt -y install python-pip &&
+sudo python -m pip install --upgrade pip &&
+echo "installing rabbitmq"
+sudo apt -y install rabbitmq-server &&
+echo "installing celery"
+sudo python -m pip install celery &&
+echo "installing openstack"
+sudo python -m pip install python-openstackclient &&
+echo "installing flask"
+sudo apt -y install python-flask
+echo "*****************************/\*********************************"
+celery --version
+openstack --version
