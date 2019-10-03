26f53d7d204c77790db031c60cfce8726ff21a6b
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-03 16:09:22 +0200

    v 0.1.0 The code creates a service on http://IP:5000/ that upon calling returns a text

--- service.py
+++ service.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1> Hello shithead </h1>"
+    return "Hello shithead"
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
