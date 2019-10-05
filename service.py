ebe0be969eda8363603b83d12749a8b2716a2ebb
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-05 15:20:19 +0200

    simple python script to be ran with flask

--- service.py
+++ service.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import time_getter
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello shithead"
+def time_print_function():
+    return time_getter.time_getter()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
---
+++ time_getter.py
@@ -0,0 +1,7 @@
+def time_getter():
+    import datetime
+    print("hi reader")
+    print("lets do some shit!")
+    print("how about time? thats a good service!")
+    print(datetime.datetime.now())
+    print("thank you, come again! (with apu's voice)")
