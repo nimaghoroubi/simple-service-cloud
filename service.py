657df801319e75f6d356528a535ce112c5810721
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-05 15:58:55 +0200

    making sth global due to a nag

--- service.py
+++ service.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def time_print_function():
     data = time_getter()
-    str = "here is your time " + str(data)
+    global str = "here is your time " + str(data)
     return str
-    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
