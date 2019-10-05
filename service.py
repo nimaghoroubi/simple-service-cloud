5d338034e185146462bbdfb1c81cb499ae9aacdd
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-05 15:53:28 +0200

    hopefully this works

--- service.py
+++ service.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def time_print_function():
     data = time_getter()
-    return data
+    print("here is your time " + str(data))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
