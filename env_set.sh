de17987e139c2e73b9c5d3f3fcd027065fa022c4
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-05 22:46:30 +0200

    test cd

--- env_set.sh
+++ env_set.sh
@@ -19,6 +19,7 @@ sudo apt -y install python-flask &&
 echo "services ready, cloning repo"
 git clone https://github.com/nimaghoroubi/simple-service-cloud &&
 echo "clone complete! running services!"
+cd ./simple-service-cloud
 python ~/simple-service-cloud/service.py &\
 celery -A service.celery worker --loglevel=info
 echo "services up and running, use http://ip:5000/<item>"
