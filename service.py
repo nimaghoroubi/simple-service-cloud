6fd248185180802024bdb9807c8a9f09010585f1
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-05 15:55:01 +0200

    it seems I have to return sth for it to work

--- service.py
+++ service.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def time_print_function():
     data = time_getter()
-    print("here is your time " + str(data))
-
+    str = "here is your time " + str(data)
+    return str
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
