47d2bfd65e0fb21ced782814e6265d0242a13863
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-05 15:25:46 +0200

    last one did not return the data, it just printed them! 2nd try

--- time_getter.py
+++ time_getter.py
@@ -1,7 +1,3 @@
 def time_getter():
     import datetime
-    print("hi reader")
-    print("lets do some shit!")
-    print("how about time? thats a good service!")
-    print(datetime.datetime.now())
-    print("thank you, come again! (with apu's voice)")
+    return datetime.datetime.now()
