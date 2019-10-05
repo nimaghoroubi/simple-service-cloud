5d2ec248007deb224bd150e82123ae94e7711cdc
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-05 16:07:19 +0200

    reducing some stuff

--- service.py
+++ service.py
@@ -1,12 +1,12 @@
 from flask import Flask
+import subprocess
 from time_getter import time_getter
 app = Flask(__name__)
 
 @app.route('/')
 def time_print_function():
     data = time_getter()
-    global item = "here is your time " + str(data)
-    return item
+    return str(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
