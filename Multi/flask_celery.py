59db769a24bea4905bbd83d1570a411914f75f05
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-19 19:34:23 +0200

    removed multi, its  in multi repo now

--- Multi/flask_celery.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-from twitter_api import twitter_api
-
-app = Celery('tweetsApp',
-             backend='rpc://',
-             broker='pyamqp://tweets:tweets@192.168.1.26/tweets')
-
-# Do tasks here
